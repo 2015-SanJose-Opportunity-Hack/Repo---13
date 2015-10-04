c2091271d05064fc679a7577be38af7fd2221a09
kvohra <kvohra@kvohras-MacBook-Pro.local>
2015-10-03 22:59:17 -0700

    first screen

--- CareForCare/app/src/main/java/com/example/kvohra/careforcare/MainActivity.java
+++ CareForCare/app/src/main/java/com/example/kvohra/careforcare/MainActivity.java
@@ -1,16 +1,133 @@
 package com.example.kvohra.careforcare;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    public static final String PREFS_NAME = "MyApp_Settings";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+     /*   SharedPreferences.Editor editor = settings.edit();
+        editor.clear();
+        editor.commit();*/
+
+        // Reading from SharedPreferences
+        String parentToken = settings.getString("parent", "");
+        String childToken = settings.getString("child", "");
+
+        if(parentToken.equalsIgnoreCase("")==false || childToken.equalsIgnoreCase("")==false)
+        {
+            if(parentToken.equalsIgnoreCase("")==false)
+            {
+                //launch as parent
+                Log.d("ourtoken",parentToken);
+            }
+            else if (childToken.equalsIgnoreCase("")==false)
+            {
+                //launch as child
+                Log.d("ourtoken",childToken);
+            }
+        }
         setContentView(R.layout.activity_main);
+
+        //if null
+        init();
+    }
+
+
+    protected void init()
+    {
+        Button parent = (Button)findViewById(R.id.buttonParent);
+        parent.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                final Dialog dialog = getDialog();
+                dialog.show();
+                
+                Button saveButton = (Button)dialog.findViewById(R.id.saveButton);
+                saveButton.setOnClickListener((new View.OnClickListener() {
+                    @Override
+                    public void onClick(View arg0) {
+                        Log.d("save", "save");
+                        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+                        //Writing data to SharedPreferences
+                        SharedPreferences.Editor editor = settings.edit();
+
+                        EditText etxtName = (EditText)dialog.findViewById(R.id.etxtName);
+                        String token = etxtName.getText().toString();
+                        editor.putString("parent", token);
+                        editor.commit();
+                        dialog.dismiss();
+                    }
+                }));
+
+                Button cancelButton = (Button)dialog.findViewById(R.id.cancelButton);
+                cancelButton.setOnClickListener((new View.OnClickListener() {
+                    @Override
+                    public void onClick(View arg0) {
+                        Log.d("cancel", "cancel");
+                        dialog.dismiss();
+                    }
+                }));
+            }
+        }));
+
+        Button child = (Button)findViewById(R.id.buttonChild);
+        child.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                final Dialog dialog = getDialog();
+                dialog.show();
+
+                Button saveButton = (Button)dialog.findViewById(R.id.saveButton);
+                saveButton.setOnClickListener((new View.OnClickListener() {
+                    @Override
+                    public void onClick(View arg0) {
+                        Log.d("save", "save");
+                        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+                        //Writing data to SharedPreferences
+                        SharedPreferences.Editor editor = settings.edit();
+                        EditText etxtName = (EditText)dialog.findViewById(R.id.etxtName);
+                        String token = etxtName.getText().toString();
+                        editor.putString("child", token);
+                        editor.commit();
+                        dialog.dismiss();
+                    }
+                }));
+
+                Button cancelButton = (Button)dialog.findViewById(R.id.cancelButton);
+                cancelButton.setOnClickListener((new View.OnClickListener() {
+                    @Override
+                    public void onClick(View arg0) {
+                        Log.d("cancel", "cancel");
+                        dialog.dismiss();
+                    }
+                }));
+            }
+        }));
+
+
+
+    }
+
+    protected Dialog getDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.parentdialogue);
+        dialog.setTitle("Save New Number");
+        dialog.setCancelable(true);
+        return dialog;
+
     }
 
     @Override
@@ -34,4 +151,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
